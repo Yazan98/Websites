8634978c7ac0412e2a0f8e1ee429e3be4f2ca233
Yazan Tarifi <yazantarifi98@gmail.com>
2020-02-01 20:12:28 +0000

    finish the counter

---
+++ counter/src/views/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="counter-container">
+    <h1>Welcome To Vue Counter</h1>
+    <h5>The First Website</h5>
+    <p>The Counter is : {{ count }}</p>
+    <button type="button" @click="count++">Increse Count</button>
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#counter-container',
+  data: function () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
