7655b0b1fb1aa147d06b68a4118d33434c6394e4
Yazan Tarifi <yazantarifi98@gmail.com>
2020-02-20 17:17:51 +0000

    add icons for services

--- personal-website/src/components/main/ServicesComponent.vue
+++ personal-website/src/components/main/ServicesComponent.vue
@@ -3,16 +3,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm">
+                    <img src="https://img.icons8.com/cotton/64/000000/android-os.png">
                     <p>Frontend Side</p>
                     <h5>Android Applications</h5>
                     <p>Design, Maintain Android Applications With Responsive Layouts , Latest Technologies, Strong Infastructure</p>
                 </div>
                 <div class="col-sm">
+                    <img src="https://img.icons8.com/cotton/64/000000/source-code--v2.png">
                     <p>Backend Side</p>
                     <h5>SpringBoot</h5>
                     <p>Build Backend Projects With Spring Boot Framework, Good Infastructure, Error Handling, Restful Api</p>
                 </div>
                 <div class="col-sm">
+                    <img src="https://img.icons8.com/cotton/64/000000/dating-website.png">
                     <p>Frontend Side</p>
                     <h5>Website</h5>
                     <p>Build Websites With Vue.js , Boostrap</p>
@@ -20,16 +23,19 @@
             </div>
             <div class="row" id="SecondItem">
                 <div class="col-sm">
+                    <img src="https://img.icons8.com/cotton/64/000000/bookmark-ribbon.png">
                     <p>Configuration</p>
                     <h5>Gradle Plugins</h5>
                     <p>Design, Build Gradle Plugins For Android Applications for Better Gradle Configutation</p>
                 </div>
                 <div class="col-sm">
+                    <img src="https://img.icons8.com/cotton/64/000000/source-code--v2.png">
                     <p>Backend Side</p>
                     <h5>Ktor Framework</h5>
                     <p>Build Backend Projects With Ktor Framework , Restful Api</p>
                 </div>
                 <div class="col-sm">
+                    <img src="https://img.icons8.com/cotton/64/000000/color-palette.png">
                     <p>Frontend Side</p>
                     <h5>Design</h5>
                     <p>Design Application Interface , Logos With Adobe Photoshop, After Effects For Simple Animation</p>
